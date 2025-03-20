974256c088c79f649ec18c0041cc180996cffb52
Gaurav-wai <gaurav.tonpe@waiin.com>
2025-03-20 12:17:00 +0000

    final prototype

--- script.js
+++ script.js
@@ -1,62 +1,7 @@
-// const arrhtml = [{
-//     img : 'dotnet',
-//     name: 'ASP.net'
-// },{
-//     img: 'devops',
-//     name: 'DevOps'
-// }
-
-
-
-// ];
-
-
-
-
-
-
-
-
-// const container = document.querySelector('.js-container');
-
-
-// if (container){
-
-// container.innerHTML = arrhtml.map (data =>`
-
-//     <div class=" col-12 col-sm-12 col-md-6 col-lg-3">
-//     <div class="colpad d-flex border rounded p-2 align-items-center">
-//         <input type="checkbox" class="me-2 flex-shrink-0"
-//                 style="height: 25px; width: 20px;">
-//             <img src="Assets/${data.img}.png" alt="asp" class="me-2 flex-shrink-0" width="32px;"
-//                 height="35px">
-//             <span class="flex-grow-1 text-truncate">${data.name}</span>
-//             <div class="trashicon d-flex">
-//                 <button class="trash btn btn-sm p-1"><img src="Assets/trash.png" alt="trash"
-//                         style="height: 20px; width: 18x;"></button>
-//             </div>
-//             <div class="pencilicon d-flex">
-//                 <button class="pencil btn btn-sm p-1 ms-1"><img src="Assets/pencil.png"
-//                         alt="pencil" style="height: 20px; width: 18x;"></button>
-//             </div>
-//         </div>
-//     </div>
-
-
-// `).join("");
-
-
-// }else{
-//     console.log("notfound");
-// }
-
-
-
-
 
 document.addEventListener("DOMContentLoaded", function () {
     const arrhtml = [
-        { img: 'HTML', name: 'HTML' },
+        { img: 'html', name: 'HTML' },
         { img: 'python', name: 'Python' },
         { img: 'css3', name: 'CSS' },
         { img: 'java', name: 'Java' },
